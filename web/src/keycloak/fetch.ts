d0578eb3b77a44973de2c53a2a0649d773c09f2e
matt <mtbarta@gmail.com>
2018-10-17 18:56:15 -0700

    #6 simplify keycloak's apollo fetch function to remove error

--- web/src/keycloak/fetch.ts
+++ web/src/keycloak/fetch.ts
@@ -10,18 +10,11 @@ export default async (uri?: string | Request, options?: any): Promise<any> => {
   let isExpired = keycloakAuth.isTokenExpired(30)
 
   if (isExpired) {
-    return keycloakAuth.updateToken(30)
-      .success((result) => {
-        let newAccessToken = keycloakAuth.token
-        options.headers.authorization = `Bearer ${newAccessToken}`
-        return fetch(uri, options)
-      })
-      .error((result) => {
-        // uh oh.
-      })
-  }
-  else {
-    return fetch(uri, options)
+    await keycloakAuth.updateToken(30)
+
+    let newAccessToken = keycloakAuth.token
+    options.headers.authorization = `Bearer ${newAccessToken}`
   }
   
+  return fetch(uri, options)
 }
